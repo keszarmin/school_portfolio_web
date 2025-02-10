e35dc7444eed96ccad01a66acce87dcd5926f179
Kész Ármin <keszarmin@gmail.com>
2025-02-10 22:52:44 +0100

    V1_fix

--- src/Map.js
+++ src/Map.js
@@ -33,7 +33,7 @@ function Map({curr}) {
         <Link onClick={() =>setFocusF("armin")} to={"/school_portfolio_web/armin"} className={Focus[1][0]} style={{textDecoration:Focus[0][0]}} >Ármin</Link>
         <Link onClick={() =>setFocusF("gergo")} to={"/school_portfolio_web/gergo"} className={Focus[1][1]} style={{textDecoration:Focus[0][2]}}>Gergő</Link>
         <Link onClick={() =>setFocusF("balazs")} to={"/school_portfolio_web/balazs"} className={Focus[1][2]} style={{textDecoration:Focus[0][1]}}>Balázs</Link>
-        <Link className='text-xl' to={"/"} >Home</Link>
+        <Link className='text-xl' to={"/school_portfolio_web"} >Home</Link>
     </nav>
     <Outlet />
     </>
